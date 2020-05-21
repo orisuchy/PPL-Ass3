344a2518d3b032fd3d9ad4423c7c8f40aad23530
Unknown <suchy@post.bgu.ac.il>
2020-05-21 17:18:21 +0300

    Before changing mapCompExp

--- part2/Mermaid.ts
+++ part2/Mermaid.ts
@@ -1,8 +1,9 @@
 import { Parsed, isExp, isProgram, isDefineExp, Program, Exp, isCExp,isNumExp, isProcExp, isPrimOp,
          isStrExp, isIfExp, isLetExp, isLitExp, isAppExp, isLetrecExp, isSetExp, isBinding, isAtomicExp,
-         isCompoundExp, AtomicExp, CompoundExp, isBoolExp, isVarRef, AppExp, makePrimOp } from "./L4-ast";
+         isCompoundExp, AtomicExp, CompoundExp, isBoolExp, isVarRef, AppExp, makePrimOp, CExp } from "./L4-ast";
 import { Result, makeFailure, makeOk } from "../shared/result";
-import { Graph, makeGraph, makeCompoundGraph, CompoundGraph, makeEdge, makeNodeDecl, Node, makeEdgeLabel, Edge } from "./Mermaid-ast";
+import { Graph, makeGraph, makeCompoundGraph, CompoundGraph, makeEdge, makeNodeDecl, Node, makeEdgeLabel, Edge, isNodeDecl, isNode } from "./Mermaid-ast";
+import { map } from "ramda";
 
 export const mapL4toMermaid = (exp: Parsed): Result<Graph> =>
     isExp(exp)? mapL4ExptoMermaid(exp) :
@@ -54,21 +55,43 @@ export const mapL4AppToMermaid(exp: AppExp): Result<CompoundGraph> =>{
     const ratorNode :Node = makeNodeDecl(makeVarGen()("PrimOp"),"PrimOp("+ratorStr+")")
     const ratorEdge :Edge = makeEdge(appExpNode, ratorNode, makeEdgeLabel("rator"))
     const randsPointerNode = makeNodeDecl(makeVarGen()("Rands"),":")
-    // for exp = (+ x 4) 
-    let xStr :string;
-    isVarRef(exp.rands[0])? xStr = exp.rands[0].var : xStr = ""
-    const xNode = makeNodeDecl(makeVarGen()("VarRef"),'(' + xStr + ')')
-    //TODO - make fucking map
-    //const randsNodes: Node[] = map(makeNodeDecl(),exp.rands)
+    const randsPtrEdge :Edge = makeEdge(appExpNode, randsPointerNode, makeEdgeLabel("rands"))
 
-    const randEdge = makeEdge() 
-    //const randsPointerEdges = map(makeEdge(randsPointerEdges,exp.rands),exp.rands) 
-    
-     
-    const edges = [ratorEdge,randEdge]
+    const randsNodes: Node[] = exp.rands.map(x => makeCexpNode(x))
+    const randsEdges: Edge[] = map(x => makeEdge(randsPointerNode,x) ,randsNodes) 
+
+    const edges = randsEdges.concat(ratorEdge,randsPtrEdge)
     return makeOk(makeCompoundGraph(edges));
 }
-export const mapL4ProgramtoMermaid = (exp: Program): Result<Graph> =>{}
+export const makeCexpNode = (exp: CExp): Node =>
+    /*
+    AtomicExp = NumExp | BoolExp | StrExp | PrimOp | VarRef;
+    CompoundExp = AppExp | IfExp | ProcExp | LetExp | LitExp | LetrecExp | SetExp;
+    */
+    isNumExp(exp)? makeNodeDecl(makeVarGen()("NumExp"),"NumExp("+exp.val+")") : 
+    isBoolExp(exp)? makeNodeDecl(makeVarGen()("BoolExp"),"BoolExp("+exp.val+")") :
+    isStrExp(exp)? makeNodeDecl(makeVarGen()("StrExp"),"StrExp("+exp.val+")"):
+    isPrimOp(exp)? makeNodeDecl(makeVarGen()("PrimOp"),"PrimOp("+exp.op+")"):
+    isVarRef(exp)? makeNodeDecl(makeVarGen()("VarRef"),"VarRef("+exp.var+")"):
+    //isCompoundExp(exp)? mapCompExp(exp):
+    makeNodeDecl("Null","Null");
+
+    
+
+
+    // isAppExp(exp)? mapL4AppToMermaid(exp):
+    // isIfExp(exp)?  mapL4IfToMermaid(exp):
+    // isProcExp(exp)? mapL4ProcToMermaid(exp):
+    // isLetExp(exp)? mapL4LetToMermaid(exp):
+    // isLitExp(exp)? mapL4LitToMermaid(exp):
+    // isLetrecExp(exp)? mapL4LetrecToMermaid(exp):
+    // isSetExp(exp)? mapL4SetAppToMermaid(exp):
+    
+
+
+    
+
+// export const mapL4ProgramtoMermaid = (exp: Program): Result<Graph> =>{}
     
     
 export const makeVarGen = (): (v: string) => string => {
