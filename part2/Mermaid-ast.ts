341af3ca51ed1c730f47f52040162ba345abf00f
Unknown <suchy@post.bgu.ac.il>
2020-05-25 15:00:46 +0300

    test 6

--- part2/Mermaid-ast.ts
+++ part2/Mermaid-ast.ts
@@ -178,7 +178,7 @@ isOk(mermaidL45)?
     console.log(mermaidL45);
 
 console.log("\n\nTest 6\n\n")
-const str6 :string ="(L4 (define f(lambda (x y)(+ x y)))(define my-list '(1 f 2)))"
+const str6 :string ="(L4 (define x (lambda(y) (- 3 y))))"
 
 const mermaidL46 = L4toMermaid(str6);
 isOk(mermaidL46)?
